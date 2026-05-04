bd753baf885f2af893963e32a0eab8c932cb3028
testedram <158482201+testedram@users.noreply.github.com>
2026-05-04 12:08:31 +0200

    Await user retrieval in login handler

--- pages/api/login.js
+++ pages/api/login.js
@@ -4,11 +4,10 @@ import { signToken } from '../../lib/auth';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end();
-
   const { username, password } = req.body;
   if (!username || !password) return res.status(400).json({ error: 'Внеси корисничко ime и лозинка' });
 
-  const user = getUserByUsername(username);
+  const user = await getUserByUsername(username);
   if (!user) return res.status(401).json({ error: 'Погрешно корисничко ime или лозинка' });
 
   const valid = await bcrypt.compare(password, user.password);
